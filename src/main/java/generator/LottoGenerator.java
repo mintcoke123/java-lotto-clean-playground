fa155e89f92e8e5f8f69280689ac1292c6e894c7
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-09-25 13:09:43 +0900

    refactor: LottoGenerator가 복수의 lotto를 생성할 수 있도록 변경

--- src/main/java/generator/LottoGenerator.java
+++ src/main/java/generator/LottoGenerator.java
@@ -1,5 +1,7 @@
 package generator;
 
+import domain.Lotto;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -9,12 +11,22 @@ public class LottoGenerator {
     private static final int ALL_LOTTO_NUMBER_SIZE = 45;
     private static final int LOTTO_NUMBER_SIZE = 6;
 
-    public List<Integer> generateLottoNumbers() {
+    public Lotto generateLottoNumbers() {
         List<Integer> allLottoNumbers = generateAllLottoNumbers();
         List<Integer> shuffledAllLottoNumbers = shuffleAllLottoNumbers(allLottoNumbers);
         List<Integer> pickedLottoNumbers = pickSixLottoNumbers(shuffledAllLottoNumbers);
-        return sortLottoNumbers(pickedLottoNumbers);
+        List<Integer> sortedLottoNumbers = sortLottoNumbers(pickedLottoNumbers);
+        return new Lotto(sortedLottoNumbers);
     }
+
+    public List<Lotto> generateLottoTickets(int count) {
+        List<Lotto> tickets = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            tickets.add(generateLottoNumbers());
+        }
+        return tickets;
+    }
+
 
     private List<Integer> pickSixLottoNumbers(List<Integer> shuffledAllLottoNumbers) {
         return new ArrayList<>(shuffledAllLottoNumbers.subList(0, LOTTO_NUMBER_SIZE));
