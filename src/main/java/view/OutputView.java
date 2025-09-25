48584d5ee17e7c204a2fa0aad89e1e776f918487
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-09-25 13:22:21 +0900

    refactor: 요구사항에 맞게 outputview 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class OutputView {
     public void printPurchasedTicketsMessage(int tickets){
+        System.out.println();
         System.out.println(tickets + "개를 구매했습니다.");
     }
 
