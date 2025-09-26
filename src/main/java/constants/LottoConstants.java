ba37c216a5624e1a0ab8b3fc792a10ad299cd105
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-09-27 00:45:04 +0900

    refactor: constants 위치 변경

--- src/main/java/constants/LottoConstants.java
+++ src/main/java/constants/LottoConstants.java
@@ -1,8 +1,6 @@
 package constants;
 
 public class LottoConstants {
-    public static final int PRICE_PER_TICKET = 1000;
-
     public static final int LOTTO_NUMBER_SIZE = 6;
     public static final int ALL_LOTTO_NUMBER_SIZE = 45;
 }
--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -11,9 +11,9 @@ import view.OutputView;
 
 import java.util.List;
 
-import static constants.LottoConstants.*;
-
 public class LottoController {
+    private static final int PRICE_PER_TICKET = 1000;
+
 
     private final InputView inputView;
     private final OutputView outputView;
