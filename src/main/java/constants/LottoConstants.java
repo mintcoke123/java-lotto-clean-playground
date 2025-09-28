2f41c2e133476ce608628fcc926369255219dfac
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-09-28 22:58:23 +0900

    refactor: 로또 도메인 로직 수정

--- src/main/java/constants/LottoConstants.java
+++
@@ -1,6 +0,0 @@
-package constants;
-
-public class LottoConstants {
-    public static final int LOTTO_NUMBER_SIZE = 6;
-    public static final int ALL_LOTTO_NUMBER_SIZE = 45;
-}
--- src/main/java/generator/LottoGenerator.java
+++ src/main/java/generator/LottoGenerator.java
@@ -2,51 +2,31 @@ package generator;
 
 import domain.Lotto;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-import static constants.LottoConstants.ALL_LOTTO_NUMBER_SIZE;
-import static constants.LottoConstants.LOTTO_NUMBER_SIZE;
+import static domain.Lotto.ALL_LOTTO_NUMBER_SIZE;
+import static domain.Lotto.LOTTO_NUMBER_SIZE;
 
 public class LottoGenerator {
 
-    public Lotto generateLottoNumbers() {
-        List<Integer> allLottoNumbers = generateAllLottoNumbers();
-        List<Integer> shuffledAllLottoNumbers = shuffleAllLottoNumbers(allLottoNumbers);
-        List<Integer> pickedLottoNumbers = pickSixLottoNumbers(shuffledAllLottoNumbers);
-        List<Integer> sortedLottoNumbers = sortLottoNumbers(pickedLottoNumbers);
-        return new Lotto(sortedLottoNumbers);
-    }
-
-    public List<Lotto> generateLottoTickets(int count) {
-        List<Lotto> tickets = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            tickets.add(generateLottoNumbers());
+    public List<Lotto> generateLottoTickets(int ticketsCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < ticketsCount; i++) {
+            lottos.add(new Lotto(generateSixLottoNumber()));
         }
-        return tickets;
+        return lottos;
     }
 
+    public List<Integer> generateSixLottoNumber() {
+        Random random = new Random();
+        Set<Integer> LottoNumberSet = new HashSet<>();
 
-    private List<Integer> pickSixLottoNumbers(List<Integer> shuffledAllLottoNumbers) {
-        return new ArrayList<>(shuffledAllLottoNumbers.subList(0, LOTTO_NUMBER_SIZE));
-    }
-
-    private List<Integer> sortLottoNumbers(List<Integer> lottoNumbers) {
+        while (LottoNumberSet.size() < LOTTO_NUMBER_SIZE) {
+            LottoNumberSet.add(random.nextInt(ALL_LOTTO_NUMBER_SIZE) + 1);
+        }
+        List<Integer> lottoNumbers = new ArrayList<>(LottoNumberSet);
         Collections.sort(lottoNumbers);
         return lottoNumbers;
     }
 
-    private List<Integer> shuffleAllLottoNumbers(List<Integer> allLottoNumbers) {
-        Collections.shuffle(allLottoNumbers);
-        return allLottoNumbers;
-    }
-
-    private List<Integer> generateAllLottoNumbers() {
-        List<Integer> lottoNumbers = new ArrayList<>();
-        for (int i = 1; i <= ALL_LOTTO_NUMBER_SIZE; i++) {
-            lottoNumbers.add(i);
-        }
-        return lottoNumbers;
-    }
 }
